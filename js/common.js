dafa4fc2c4f14d33f8ee22617beec487f82477cc
MogilevichDmitry <dmsmain@gmail.com>
2016-08-09 19:09:57 +0300

    Add Home and My Works blocks

---
+++ js/common.js
@@ -0,0 +1,52 @@
+$( document ).ready(function() {
+    /* scroll onclick */
+    $(function () {
+        $('.arrow-down').on('click', function (e) {
+            $('html,body').stop().animate({scrollTop: $('#portfolio').offset().top}, 650, 'swing');
+            e.preventDefault();
+        });
+
+    });
+
+    /* scroll on scroll */
+    var anchors = [];
+    var currentAnchor = 0;
+    var isAnimating  = false;
+
+    $(function(){
+        function updateAnchors() {
+            anchors = [];
+            $('.anchor').each(function(i, element) {
+                anchors.push( $(element).offset().top );
+            });
+        }
+
+        $('body').on('mousewheel', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            if( isAnimating ) {
+                return false;
+            }
+            isAnimating  = true;
+            // Increase or reset current anchor
+            if( e.originalEvent.wheelDelta >= 0 ) {
+                currentAnchor--;
+            } else {
+                currentAnchor++;
+            }
+            if( currentAnchor > (anchors.length - 1)
+                || currentAnchor < 0 ) {
+                currentAnchor = 0;
+            }
+            isAnimating  = true;
+            $('html, body').animate({
+                scrollTop: parseInt( anchors[currentAnchor] )
+            }, 650, 'swing', function() {
+                isAnimating  = false;
+            });
+        });
+
+        updateAnchors();
+
+    });
+});
