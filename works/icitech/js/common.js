c22ef72baa1660d9ef3cb9f22972b1016c13566c
MogilevichDmitry <dmsmain@gmail.com>
2016-08-17 17:55:12 +0300

    Fix Icitech feedback-btn, add fixed borders

--- works/icitech/js/common.js
+++ works/icitech/js/common.js
@@ -1,26 +1,5 @@
 $( document ).ready(function() {
-    $(function () {
-        $("#typed-tagline").typed({
-            strings: [
-                "Imagine. Create. Improve",
-                "Нужен сайт? Мы сделаем",
-                "Уникальный дизайн",
-                "Грамотный код",
-                "Поддержка и гарантия",
-                "Остались вопросы? Звоните +375 33 332 19 44",
-                "Ну, или читайте",
-                "Мы молодая и дружная команда",
-                "Творчески подходим к заданию",
-                "С пользой для бизнеса",
-                "И не боимся сложных задач!",
-                "Наш подход к делу:"
-            ],
-            typeSpeed: 35,
-            backDelay: 500,
-            loop: true,
-        });
-    });
-    $(window).bind('scroll', function () {
+   /* $(window).bind('scroll', function () {
         if ($(window).width() > 1040) {
             if ($(window).scrollTop() > 450) {
                 $('.common__feedback-btn').fadeIn(250);
@@ -28,5 +7,5 @@ $( document ).ready(function() {
                 $('.common__feedback-btn').fadeOut(250);
             }
         }
-    });
+    });*/
 });
