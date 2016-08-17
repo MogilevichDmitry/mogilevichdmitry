cee75c3b2e58860318773ebaeeaf9155b50fd629
MogilevichDmitry <dmsmain@gmail.com>
2016-08-17 12:28:09 +0300

    Fix layout icitech

--- works/icitech/js/common.js
+++ works/icitech/js/common.js
@@ -23,9 +23,9 @@ $( document ).ready(function() {
     $(window).bind('scroll', function () {
         if ($(window).width() > 1040) {
             if ($(window).scrollTop() > 450) {
-                $('.common__feedback-btn').fadeIn(200);
+                $('.common__feedback-btn').fadeIn(250);
             } else {
-                $('.common__feedback-btn').fadeOut(200);
+                $('.common__feedback-btn').fadeOut(250);
             }
         }
     });
